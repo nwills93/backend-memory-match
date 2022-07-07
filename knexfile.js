305638ce366d77506c3acb6c77945d789d344f84
Nathaniel Wills <nathanielwills708@gmail.com>
2022-07-07 13:47:00 -0700

    updated kenxfile for prod env

--- knexfile.js
+++ knexfile.js
@@ -37,17 +37,16 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: DATABASE_URL,
     pool: {
-      min: 2,
-      max: 10
+      min: 0,
+      max: 5
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: path.join(__dirname, "src", "db", "migrations")
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds")
     }
   }
 
